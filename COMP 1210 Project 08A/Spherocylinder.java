685417e2dcc0846007602c1084b0f3a832f194a7
mdj0025 <mdj0025@tigermail.auburn.edu>
2019-01-02 15:23:15 -0600

    Add files via upload

---
+++ COMP 1210 Project 08A/Spherocylinder.java
@@ -0,0 +1,188 @@
+import java.text.DecimalFormat;
+ /**
+  * This class contains constructors and variables for the program.
+  *
+  * Project 08A
+  * Michael Johnson Comp1210 Section 003
+  * 11-02-17
+  */
+  
+ public class Spherocylinder
+ {
+    // instance variables
+    private String label = "";
+    private double radius = 0;
+    private double cylinderHeight = 0;
+    private static int count = 0;
+    
+    /** This constructor stores and renames variables.
+     * @param labelIn -- renames label
+     * @param radiusIn -- renames radius
+     * @param cylinderHeightIn -- renames cylinderHeight
+     */
+    public Spherocylinder(String labelIn, double radiusIn, 
+       double cylinderHeightIn)
+    {  
+       setLabel(labelIn);
+       setRadius(radiusIn);
+       setCylinderHeight(cylinderHeightIn);
+       count++;
+    }
+    /**
+     * This string method accepts no parameters and returns label.
+     * @return -- returns label
+     */
+    public String getLabel()
+    {  
+    
+       return label;
+    
+    }
+    /** This method checks is the label is empty, if not it trims the label.
+     * @param labelIn -- used in boolean condition
+     * @return -- returns a boolean condition
+     */
+    public boolean setLabel(String labelIn)
+    { 
+       boolean isSet = false;
+       if (labelIn != null)
+       {
+          label = labelIn.trim();
+          isSet = true;
+       }
+       
+       return isSet;
+    }
+    /** This method accepts no parameters and returns the radius.
+     * @return -- returns the radius
+     */ 
+    public double getRadius()
+    {
+       return radius;
+    }
+    /** This method checks if the radius is positive.
+     * @param radiusIn -- used in boolean condition.
+     * @return -- returns a boolean condition.
+     */
+    public boolean setRadius(double radiusIn)
+    {
+       boolean isSet = false;
+       if (radiusIn >= 0)
+       {
+          radius = radiusIn;
+          isSet = true;
+       
+       }
+       return isSet;
+    }
+    /** This method returns the cylinder height.
+     * @return -- returns cylinderHeight
+     */
+    public double getCylinderHeight()
+    {
+       return cylinderHeight;
+    }
+    /** This method checks if the cylinder height is positive.
+     * @param cylinderHeightIn -- used in boolean condition
+     * @return -- returns a boolean condition
+     */
+    public boolean setCylinderHeight(double cylinderHeightIn)
+    {
+       boolean isSet = false;
+       if (cylinderHeightIn >= 0)
+       {
+          cylinderHeight = cylinderHeightIn;
+          isSet = true;
+       }
+       
+       return isSet;
+    }
+ 
+    /** This method defines the circumference of the sphyrocylinder.
+     * @return -- returns the circumference value
+     */
+    public double circumference()
+    {
+       double circumference = 2.0 * Math.PI * radius;
+        
+       return circumference;
+    }
+    /** This method calculates the value for the surface area.
+     *@return -- returns the surface area
+     */
+    public double surfaceArea()
+    {
+       double surfaceArea = 2 * Math.PI * radius * (2 * radius + cylinderHeight);
+       return surfaceArea;
+    }
+    /** This method calculates the volume.
+     * @return -- returns the volume
+     */
+    
+    public double volume()
+    { 
+       double volume = Math.PI * Math.pow(radius, 2) 
+          * ((4.0 / 3.0) * radius + cylinderHeight);
+       return volume;
+    }
+    
+    /** This method takes in calculated values and puts them in a string.
+     * @return -- returns output
+     */
+    public String toString()
+    {
+       DecimalFormat fmt = new DecimalFormat("#,##0.00#");
+       
+       String output = "Spherocylinder \"" + label + "\" with radius " + radius 
+          + " and cylinder height " + cylinderHeight + " has:";
+       output += "\n\tcircumference = " + fmt.format(circumference()) + " units";
+       output += "\n\tsurface area = " + fmt.format(surfaceArea()) 
+          + " square units";
+       output += "\n\tvolume = " + fmt.format(volume()) + " cubic units";
+            
+       return output;
+    }
+    
+    /** This method is an accessor method for count.
+     * @return -- returns count
+     */
+    public static int getCount()
+    {
+       return count;
+    }
+    
+    /** This method resets the count variable.
+     */
+    public static void resetCount()
+    {
+       count = 0;
+    }
+    
+    /** This method determines if two spherocylinder objects are equal.
+     * @param obj -- spherocylinder object.
+     * @return -- returns boolean condition.
+     */
+    
+    public boolean equals(Object obj) {
+    
+       if (!(obj instanceof Spherocylinder)) { 
+          return false;
+       }
+       else {
+          Spherocylinder d = (Spherocylinder) obj; 
+          return (label.equalsIgnoreCase(d.getLabel())
+             
+             && Math.abs(radius - d.getRadius()) < .000001)
+             && Math.abs(cylinderHeight - d.getCylinderHeight())
+             < .000001;
+       }
+    }
+    /** This method holds the classes's hash code.
+     * @return -- returns zero.
+     */
+    public int hashCode()
+    {
+       return 0;
+    }
+ 
+ }
---
+++ COMP 1210 Project 08A/SpherocylinderTest.java
@@ -0,0 +1,179 @@
+import org.junit.Assert;
+ //import static org.junit.Assert.*;
+ import org.junit.Before;
+ import org.junit.Test;
+ 
+ 
+ /** This is a test class for the Spherocylinder object.
+  *
+  * Project 08A
+  * Michael Johnson Comp1210 Section 003
+  * 11-02-17
+  */
+ public class SpherocylinderTest {
+ 
+ 
+    /** Fixture initialization (common initialization
+     *  for all tests). **/
+    @Before public void setUp() 
+    {
+    }
+ 
+    /** This method tests the getLabel method.
+     */
+    @Test public void getLabelTest() 
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals("Test Example", sphero.getLabel());     
+    }
+    
+    /** This method tests the setLabel method.
+     */
+    @Test public void setLabelFalseTest()
+    {
+       Spherocylinder sphero = new Spherocylinder(null, 0.0, 0.0);
+       Assert.assertEquals(false, sphero.setLabel(null));
+    }
+    
+    /** This method tests the setLabel method.
+     */
+    @Test public void setLabelTrueTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(true, sphero.setLabel("Test Example"));
+    }
+    
+    /** This method tests the getRadius method.
+     */
+    @Test public void getRadiusTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(16.0, sphero.getRadius(), .000001);
+    }
+    
+    /** This method tests the setRaduis method and makes sure it fails.
+     */
+    @Test public void setRadiusFalseTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", -2, 22.0);
+       Assert.assertEquals(false, sphero.setRadius(-2));
+    }
+    
+    /** This method tests the setRadius method.
+     */
+    @Test public void setRadiusTrueTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(true, sphero.setRadius(2));
+    } 
+    
+    /** This method tests the getCylinderHeight method.
+     */
+    @Test public void getCylinderHeightTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(22.0, sphero.getCylinderHeight(), .000001);
+    }
+    
+    /** This method tests the setCylinderHeight method and make sure it fails.
+     */
+    @Test public void setCylinderHeightFalseTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, -22.0);
+       Assert.assertEquals(false, sphero.setCylinderHeight(-22.0));
+    }
+    
+    /** This method tests the circumference method.
+     */
+    @Test public void circumferenceTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(100.530964, sphero.circumference(), .000001);
+    }
+    
+    /** This method tests the surfaceArea method.
+     */
+    @Test public void surfaceAreaTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(5428.672105, sphero.surfaceArea(), .000001);
+    }
+    
+    /** This method tests the volume method.
+     */
+    @Test public void volumeTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(34850.734503, sphero.volume(), .000001);
+    }
+    
+    /** This method tests the toString method.
+     */
+    @Test public void toStringTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals("Spherocylinder \"Test Example\" with radius 16.0 and"
+          + " cylinder height 22.0 has:\n\tcircumference = 100.531"
+          + " units\n\tsurface"
+          + " area = 5,428.672 square units\n\tvolume = 34,850.735 cubic units",
+          sphero.toString());
+    }
+    
+    /** This method tests the getCount method.
+     */
+    @Test public void getCountTest()
+    {
+       
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       sphero.resetCount();
+       Assert.assertEquals(0, sphero.getCount());  
+    }
+    
+    /** This method tests the resetCount method.
+     */
+    @Test public void resetCountTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Spherocylinder sphero12 = new Spherocylinder("Test Example", 165.0,
+           2255.0);
+       sphero.resetCount();
+       Assert.assertEquals(0, sphero.getCount());
+    
+    }
+    
+    /** This method tests the equals method and makes sure it fails.
+     */
+    @Test public void equalsFalseTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Spherocylinder sphero12 = new Spherocylinder("Tet Example", 16.0, 22.0);
+       Assert.assertEquals(false, sphero.equals(sphero12));
+       
+       Spherocylinder sphero13 = new Spherocylinder("Test Example", 12.0, 22.0);
+       Assert.assertEquals(false, sphero.equals(sphero13));
+       
+       Spherocylinder sphero14 = new Spherocylinder("Test Example", 16.0, 2.0);
+       Assert.assertEquals(false, sphero.equals(sphero14));
+       
+       Assert.assertEquals(false, sphero.equals("not an object"));
+         
+    }
+    
+    /** This method tests the equals method.
+     */
+    @Test public void equalsTrueTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Spherocylinder sphero34 = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(true, sphero.equals(sphero34));
+    }
+    
+    /** This method tests the hashCode method.
+     */
+    @Test public void hashCodeTest()
+    {
+       Spherocylinder sphero = new Spherocylinder("Test Example", 16.0, 22.0);
+       Assert.assertEquals(0, sphero.hashCode());
+    }
+    
+ }
